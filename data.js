29e57c31c1461cfb8706235f17506cfaf4deadfd
ndh7296 <dhlrkrwleo@gmail.com>
2024-10-16 11:20:03 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,103 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-18",
+      "name": "파노라마18",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -0.22655809977025676,
+        "pitch": 0.0035271523186466425,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.777782222659388,
+          "pitch": -0.02243114821780523,
+          "rotation": 12.566370614359176,
+          "target": "1-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.41849443155886945,
+          "pitch": 0.25910968033131887,
+          "title": "요양원 2층 내부",
+          "text": "요양원 객실 벽으로 다 막혀있어 답답해 중앙을 뚫어줌"
+        },
+        {
+          "yaw": 1.671371153596315,
+          "pitch": 0.09564437126067205,
+          "title": "손잡이",
+          "text": "어르신들 이동을 보조해주는 손잡이를 설치함"
+        },
+        {
+          "yaw": -0.613240934896794,
+          "pitch": 0.01579539926047957,
+          "title": "Title",
+          "text": "창밖을보며 담소를 나누거나 답답함을 해소할수있는 영역을 만듦"
+        },
+        {
+          "yaw": -3.0685117052827096,
+          "pitch": 0.11851526238512733,
+          "title": "보호사 관리",
+          "text": "양쪽에서 중앙을 바라보게 자리를 두어 어르신들을 관리"
+        }
+      ]
+    },
+    {
+      "id": "1-3",
+      "name": "파노라마3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.23388332066752326,
+          "pitch": 0.013980292138494832,
+          "rotation": 0,
+          "target": "0-18"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.07848289645763096,
+          "pitch": 0.555572491278955,
+          "title": "건물 뒤 산책로",
+          "text": "주차장과 연결되어있음"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
